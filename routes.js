c3e4609d242ad0d6ac22150268d629365a0cdc81
supremeboca <63063349+supremeboca@users.noreply.github.com>
2020-05-13 18:25:11 +0200

    Add files via upload

--- routes.js
+++ routes.js
@@ -6,28 +6,72 @@ let router = express.Router();
 var playlistController = require('./controllers/playlistController');
 var playlistApiController = require ('./controllers/playlistApiController');
 
-router.get('/', (request, response) => response.redirect('/playlist'));
 
-router.get('/playlist', playlistController.playlistList);
+let check = (req,res, next) => {
+
+    if( req.session && req.session.userid >= 0) {
+        
+        next();
+    
+    }
+    else{
+        res.send('Acces denied')
+    }
+};
+
+
+
+
+router.get('/', (request, response) => response.redirect('/login_form'));
+
+
+router.get('/playlist',check,playlistController.playlistList);
 router.get('/playlist/add', playlistController.playlistFormAdd);
 router.post('/playlist/new', playlistController.playlistNew);
 router.get('/playlist/update/:playid', playlistController.playlistFormUpdate);
 router.get('/playlist/delete/:playid', playlistController.playlistRemove);
 
-router.get('api/playlist', playlistApiController.playlistList);
-router.post('api/playlist', playlistApiController.playlistNew);
-router.put('api/playlist/:playid', playlistApiController.playlistUpdate);
-router.delete('api/playlist/:playid',playlistApiController.playlistRemove);
+router.get('/api/playlist', playlistApiController.playlistList);
+router.get('/api/playlist/:userid', playlistApiController.playList);
+router.post('/api/playlist', playlistApiController.playlistNew);
+router.put('/api/playlist/:playid', playlistApiController.playlistUpdate);
+router.delete('/api/playlist/:playid',playlistApiController.playlistRemove);
 
 var songController = require('./controllers/songController');
-
+var songApiController = require ('./controllers/songApiController');
 router.get('/', (request, response) => response.redirect('/song'));
 
 router.get('/song/:playid', songController.songList);
 router.get('/song', songController.songAllList);
-router.get('/song/:playid/add', songController.songFormAdd);
+router.get('/song/add/:playid', songController.songFormAdd);
 router.post('/song/new', songController.songNew);
 router.get('/song/update/:songid', songController.songFormUpdate);
 router.get('/song/delete/:songid', songController.songRemove);
 
- module.exports = router;
+router.get('/api/song/:playid', songApiController.songList);
+router.get('/api/song', songApiController.songAllList);
+router.post('/api/song', songApiController.songNew);
+router.put('/api/song/:songid', songApiController.songUpdate);
+router.delete('/api/song/:songid', songApiController.songRemove);
+
+
+
+var UserController = require('./controllers/UserController');
+var UserApiController = require ('./controllers/UserApiController');
+router.get('/', (request, response) => response.redirect('/user'));
+
+router.get('/user', UserController.userList);
+router.get('/register_form', UserController.userFormAdd);
+router.post('/register_save', UserController.userNew);
+router.get('/user/update/:userid', UserController.userFormUpdate);
+router.get('/user/delete/:userid', UserController.userRemove);
+
+router.get('/login_form',UserController.loginForm);
+router.get("/logout",UserController.logout);
+
+
+router.get('/api/user/:userid', UserApiController.userList);
+router.post('/api/user', UserApiController.userNew);
+router.put('/api/user/:userid', UserApiController.userUpdate);
+router.delete('/api/user/:userid',UserApiController.userRemove);
+module.exports = router;
